084d4fa1a9f08b983721681f9568922a1ee3e366
AntonyLajes <tonymateuslajes@hotmail.com>
2025-02-14 14:37:02 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import fastify from 'fastify'
+import { knex } from './database'
+import { randomUUID } from 'node:crypto'
+import { env } from './env'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  
+  const transaction = await knex('transactions').insert({
+    id: randomUUID(),
+    title: 'Transação de teste',
+    amount: 1000
+  }).returning('*')
+
+  return transaction
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => console.log("HTTP server running!"))
